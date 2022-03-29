89242274fbaa70439a70750b0f37d4d41a53b6a1
Thales <82174699+yFrosttty@users.noreply.github.com>
2022-03-29 15:18:15 -0300

    v1.0

---
+++ src/ExercicioDeEstruturaIF/ExercicioIf05.java
@@ -0,0 +1,29 @@
+package ExercicioDeEstruturaIF;
+
+import java.util.Scanner;
+
+public class ExercicioIf05 {
+
+    static final Integer VALOR_PARA_CALCULAR_A_MEDIA = 2;
+    static final Integer MEDIA_PARA_SER_APROVADO_OU_REPROVADO = 7;
+    static final Integer NOTA_MAXIMA_DO_ALUNO = 10;
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do aluno(a) (っ◕‿◕)っ\n-->  ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor digite a primeira nota do aluno(a) (っ◕‿◕)っ\n--> ");
+        int primeiraNotaDoAluno = scanner.nextInt();
+
+        System.out.println("Por favor digite a segunda nota do aluno(a) (っ◕‿◕)っ\n--> ");
+        int segundaNotaDoAluno = scanner.nextInt();
+
+        int mediaDanotaDoAluno = (primeiraNotaDoAluno + segundaNotaDoAluno) / VALOR_PARA_CALCULAR_A_MEDIA;
+
+        boolean mediaMaior = primeiraNotaDoAluno >= MEDIA_PARA_SER_APROVADO_OU_REPROVADO;
+        boolean mediaMenor = primeiraNotaDoAluno >= NOTA_MAXIMA_DO_ALUNO;
+    }
+}
