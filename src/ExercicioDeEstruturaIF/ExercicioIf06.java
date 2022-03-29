b8436f72718ce9e37397d00e5a5c1dac7e9df69e
Thales <82174699+yFrosttty@users.noreply.github.com>
2022-03-29 16:59:04 -0300

    v1.0

---
+++ src/ExercicioDeEstruturaIF/ExercicioIf06.java
@@ -0,0 +1,47 @@
+package ExercicioDeEstruturaIF;
+
+import java.util.Scanner;
+
+public class ExercicioIf06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor A: ");
+        int valorA = scanner.nextInt();
+
+        System.out.println("Digite o valor B: ");
+        int valorB = scanner.nextInt();
+
+        System.out.println("Digite o valor C: ");
+        int valorC = scanner.nextInt();
+
+        boolean valorAMaior = obterValorAMaior(valorA, valorB, valorC);
+        boolean valorBMaior = obterValorBMaior(valorA, valorB, valorC);
+        boolean valorCMaior = obterValorCMaior(valorA, valorB, valorC);
+
+        if (valorAMaior) {
+            System.out.println("valor 'A' é maior");
+        }
+        else if (valorBMaior) {
+            System.out.println("valor 'B' é maior");
+        }
+        else if (valorCMaior) {
+            System.out.println("valor 'C' é maior");
+        }
+        else {
+            System.out.println("Existe notas iguais");
+        }
+    }
+
+    private static boolean obterValorAMaior(int valorA, int valorB, int valorC) {
+        return valorA > valorB && valorA > valorC;
+    }
+
+    private static boolean obterValorBMaior(int valorA, int valorB, int valorC) {
+        return valorB > valorA && valorB > valorC;
+    }
+
+    private static boolean obterValorCMaior(int valorA, int valorB, int valorC) {
+        return valorC > valorA && valorC > valorB;
+    }
+}
