9d4bbe9a7e7c1455c6b28e6aa593097cb0d05d82
I-am-Samuel <samueleduardorocha@gmail.com>
2022-03-29 22:35:45 -0300

    v1.0

---
+++ src/ExercicioDeEstruturaIF/ExercicioIf08.java
@@ -0,0 +1,9 @@
+package ExercicioDeEstruturaIF;
+
+import java.util.Scanner;
+
+public class ExercicioIf08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
