ba3a321a0351b6ce3f4e8c5ffca6648fe503df33
Thales <82174699+yFrosttty@users.noreply.github.com>
2022-03-31 17:03:05 -0300

    v1.0

---
+++ src/ExercicioDeEstruturaIF/ExercicioIf12.java
@@ -0,0 +1,15 @@
+package ExercicioDeEstruturaIF;
+
+import java.util.Scanner;
+
+public class ExercicioIf12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a sua carga horaria --> ");
+        int cargaHoraria = scanner.nextInt();
+
+        System.out.println("Digite a quantidade de horas trabalhadas por mês --> ");
+        int horasTrabalhadasNoMes = scanner.nextInt();
+    }
+}
