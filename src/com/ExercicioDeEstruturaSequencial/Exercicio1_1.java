68f63d0601abe8e76a02b7aec0a3f054b5082dfb
Thales <82174699+yFrosttty@users.noreply.github.com>
2022-04-01 16:58:35 -0300

    AtividadeBlaBla

---
+++ src/com/ExercicioDeEstruturaSequencial/Exercicio1_1.java
@@ -0,0 +1,11 @@
+package com.ExercicioDeEstruturaSequencial;
+
+import  java.util.Scanner;
+
+public class Exercicio1_1 {
+    public static void main(String() args); {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira o primero número");
+        Double
+    }
+}
